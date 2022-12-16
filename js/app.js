543a092bdc33e3cce07511ee1ce848d65071e8e7
Ziad Hosny <ziadhosny007@gmail.com>
2022-12-16 14:11:09 +0200

    add icon

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
 const sections = document.getElementsByTagName('section');
 const ul = document.getElementById('navbar__list');
 const ul_array = document.getElementById('navbar__list').childNodes;
@@ -32,24 +8,9 @@ const sectionOffsetTopVAL = [];
 let activeSection;
 let activeSectionIndex;
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
 const bulidNav = () => {
   for (let section of sections) {
     let li = section.attributes['data-nav'].nodeValue;
-    //console.log(li);
     ul.innerHTML += `<li><a href="" class=menu__link> ${li}</li>`;
     sectionOffsetTopVAL.push(section.offsetTop);
   }
@@ -57,18 +18,6 @@ const bulidNav = () => {
 };
 
 bulidNav();
-
-console.log(sectionOffsetTopVAL);
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
 
 window.addEventListener('scroll', (e) => {
   if (sectionOffsetTopVAL && window.scrollY >= sectionOffsetTopVAL[0]) {
@@ -101,7 +50,6 @@ burgerMenu.addEventListener('click', (e) => {
 // Scroll to section on link click
 ul.addEventListener('click', (e) => {
   e.preventDefault();
-  //console.dir(e.target.nodeName);
   console.log(e.target.outerText);
   if (e.target.nodeName == 'A') {
     for (let section of sections) {
@@ -116,4 +64,3 @@ ul.addEventListener('click', (e) => {
   }
 });
 
-// Set sections as active
